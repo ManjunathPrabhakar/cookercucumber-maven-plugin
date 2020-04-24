fe32f24011cb28aa90ff4ed83612882f8eda3de2
ManjunathPrabhakar <manjunath189@gmail.com>
2020-04-24 15:05:47 +0530

    cleaned excel reader & scenario Outline

---
+++ src/main/java/CookerCucumberMavenPlugin/ExceptionsFactory/Exceps/ExcelException.java
@@ -0,0 +1,15 @@
+package CookerCucumberMavenPlugin.ExceptionsFactory.Exceps;
+
+import CookerCucumberMavenPlugin.ExceptionsFactory.CookerPluginException;
+
+/**
+ * @author Manjunath-PC
+ * @created 24/04/2020
+ * @project cookercucumber-maven-plugin
+ */
+public class ExcelException extends CookerPluginException {
+
+    public ExcelException(String message) {
+        super(message);
+    }
+}
